5d2f9bde3ffc0ce858f3b331517c25699c4e8f93
Андрій Лис <andriu2000pro@gmail.com>
2026-01-28 23:12:51 +0200

    Fix: move route to routers, add env config

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI
 import uvicorn
+from dotenv import load_dotenv
+import os
+from app.routers import health
+
+load_dotenv()
 
 app=FastAPI()
-@app.get("/")
-def health_check():
-    return {
-        "status_code": 200,
-        "detail": "ok",
-        "result": "working"
-}
+
+app.include_router(health.router)
+
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+    HOST = os.getenv("APP_HOST", "127.0.0.1")
+    PORT = int(os.getenv("APP_PORT", 8000))
+    
+    uvicorn.run("app.main:app", host=HOST, port=PORT, reload=True)
--- tests/test_main.py
+++ tests/test_main.py
@@ -4,7 +4,7 @@ from app.main import app
 client= TestClient(app)
  
 def test_health_check():
-    response = client.get("/")
+    response = client.get("/healthcheck")
     assert response.status_code == 200
     assert response.json()=={
          "status_code": 200,
