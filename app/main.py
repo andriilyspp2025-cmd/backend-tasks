04b6d3f41695c029560014269cebf428dd53a572
Андрій Лис <andriu2000pro@gmail.com>
2026-02-02 11:17:41 +0200

    Fixed: replace os.getenv with PydanticSettings

--- app/main.py
+++ app/main.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI
 import uvicorn
-from dotenv import load_dotenv
-import os
+from app.core.config import settings
 from app.routers import health
 
-load_dotenv()
 
 app=FastAPI()
 
 app.include_router(health.router)
 
 if __name__ == "__main__":
-    HOST = os.getenv("APP_HOST", "127.0.0.1")
-    PORT = int(os.getenv("APP_PORT", 8000))
-    
-    uvicorn.run("app.main:app", host=HOST, port=PORT, reload=True)
+    uvicorn.run(
+        "app.main:app", 
+        host=settings.APP_HOST, 
+        port=settings.APP_PORT, 
+        reload=True
+    )
